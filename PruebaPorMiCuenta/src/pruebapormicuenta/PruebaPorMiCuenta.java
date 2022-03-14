4aff16423eb274a026efa04c32c36f5124f0ad78
Pedro <>
2022-03-14 17:55:33 +0100

    he añadido un sout diciendo que es un proyecto hecho por mi cuenta

---
+++ PruebaPorMiCuenta/src/pruebapormicuenta/PruebaPorMiCuenta.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebapormicuenta;
+
+/**
+ *
+ * @author Pedro
+ */
+public class PruebaPorMiCuenta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Esto es una prueba que hice por mi cuenta con el gif.");
+    }
+    
+}
